8c44c092b5988298c654e821a7a0e1180ea6f36f
markogeorgiev <markogeorgiev12@gmail.com>
2025-07-15 01:20:06 +0200

    Better docstart.sh FIX

--- docstart.sh
+++ docstart.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+# Stop and remove all containers
+for container in $(docker ps -aq); do
+    docker rm -f "$container"
+done
+
+# Rebuild and recreate containers
 docker compose build
 docker compose up -d --force-recreate
