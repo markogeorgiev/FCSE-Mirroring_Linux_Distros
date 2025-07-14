5f2f64237312dee0797c36d803418978ddaf8df6
markogeorgiev <markogeorgiev12@gmail.com>
2025-07-15 00:25:22 +0200

    Stupid VSCODE fixed

---
+++ debian-10/sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "[$(date)] sync.sh started" >> /var/log/sync.log
+
+MIRROR_DIR="/var/www/html/debian-10"
+REMOTE="rsync://deb.debian.org/debian/"
+
+mkdir -p "$MIRROR_DIR"
+rsync -rtlH --delete-after --delay-updates --safe-links --progress "$REMOTE" "$MIRROR_DIR" >> /var/log/sync.log 2>&1
