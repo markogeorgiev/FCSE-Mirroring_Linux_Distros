e5fbe01ae4be6f4c7e655f23d3ddfdca1ce3bfde
markogeorgiev <markogeorgiev12@gmail.com>
2025-07-15 12:19:58 +0200

    Recrusive add all files in centOS

--- centos/sync.sh
+++ centos/sync.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 set -e
 
 LOG="/var/log/sync.log"
@@ -7,16 +6,16 @@ BASE="/var/www/html/centos"
 VAULT="rsync://archive.kernel.org/centos-vault"
 
 echo "[$(date)] sync.sh started" >> "$LOG"
-
 mkdir -p "$BASE/5.0" "$BASE/6.10" "$BASE/7.0.1406"
 
-rsync -rtlH --delete-after --delay-updates --safe-links --progress \
-  "$VAULT/5.0/" "$BASE/5.0/" >> "$LOG" 2>&1
+for ver in 5.0 6.10 7.0.1406; do
+  echo "[$(date)] Syncing CentOS $ver" >> "$LOG"
 
-rsync -rtlH --delete-after --delay-updates --safe-links --progress \
-  "$VAULT/6.10/" "$BASE/6.10/" >> "$LOG" 2>&1
-
-rsync -rtlH --delete-after --delay-updates --safe-links --progress \
-  "$VAULT/7.0.1406/" "$BASE/7.0.1406/" >> "$LOG" 2>&1
+  rsync -a --delete --delay-updates --safe-links --progress \
+    "$VAULT/$ver/" "$BASE/$ver/" >> "$LOG" 2>&1 \
+    || {
+      echo "[$(date)] ERROR syncing $ver" >> "$LOG"
+    }
+done
 
 echo "[$(date)] sync.sh finished" >> "$LOG"
