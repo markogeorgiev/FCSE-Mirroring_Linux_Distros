e1b177913534cc6217465af7a81363221423d58a
markogeorgiev <markogeorgiev12@gmail.com>
2025-07-15 12:28:03 +0200

    Recrusive add all files in centOS pt3

--- centos/sync.sh
+++ centos/sync.sh
@@ -10,7 +10,7 @@ mkdir -p "$BASE/5.0" "$BASE/6.10" "$BASE/7.0.1406"
 
 for ver in 5.0 6.10 7.0.1406; do
   echo "[$(date)] Syncing CentOS $ver" >> "$LOG"
-  rsync -a --delete --delay-updates --safe-links --progress \
+  rsync -av --delete --delay-updates --safe-links --progress \
     "$VAULT/$ver/" "$BASE/$ver/" >> "$LOG" 2>&1 \
     || echo "[$(date)] ERROR syncing $ver" >> "$LOG"
 done
