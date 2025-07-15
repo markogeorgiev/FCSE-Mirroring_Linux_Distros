b7fd66e3b49f37279aa4188c32bee84bf0eb526b
markogeorgiev <markogeorgiev12@gmail.com>
2025-07-15 02:21:44 +0200

    better sync for deb-10 and deb-11

--- debian-11/sync.sh
+++ debian-11/sync.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-echo "[$(date)] sync.sh started" >> /var/log/sync.log
+echo "[$(date)] sync-debian-11.sh started" >> /var/log/sync.log
 
 MIRROR_DIR="/var/www/html/debian-11"
 REMOTE="rsync://ftp.debian.org/debian/"
+DIST="bullseye"
 
-mkdir -p "$MIRROR_DIR"
-rsync -rtlH --delete-after --delay-updates --safe-links --progress "$REMOTE" "$MIRROR_DIR" >> /var/log/sync.log 2>&1
+mkdir -p "$MIRROR_DIR/dists"
+mkdir -p "$MIRROR_DIR/pool"
+
+echo "[$(date)] Syncing dists/$DIST ..." >> /var/log/sync.log
+rsync -rtlH --delete-after --delay-updates --safe-links --progress \
+  "${REMOTE}dists/$DIST" "$MIRROR_DIR/dists" >> /var/log/sync.log 2>&1
+
+echo "[$(date)] Syncing pool/main ..." >> /var/log/sync.log
+rsync -rtlH --delete-after --delay-updates --safe-links --progress \
+  "${REMOTE}pool/main" "$MIRROR_DIR/pool/main" >> /var/log/sync.log 2>&1
+
+echo "[$(date)] sync-debian-11.sh completed" >> /var/log/sync.log
