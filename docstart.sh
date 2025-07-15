02b6f368063ef7c5210d0a4f329defb92dc26044
markogeorgiev <markogeorgiev12@gmail.com>
2025-07-15 03:36:39 +0200

    Better debian for 11 and 12 (no 10)

--- docstart.sh
+++ docstart.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -e
 
-# Stop and remove all containers
-for container in $(docker ps -aq); do
-    docker rm -f "$container"
-done
-
-# Rebuild and recreate containers
 docker compose build
 docker compose up -d --force-recreate
